be27440ac4a3cfc5de8651d1fbe45aff0f561a15
HridoyAlam <optimus5289@gmail.com>
2022-04-17 14:51:30 +0600

    static file

--- stocks/app/views.py
+++ stocks/app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html", {})
+
+def about(request):
+    return render(request, "about.html", {})
+
+
+# pk_0f23852bc2214eecb55dea7c1ffb8cba
