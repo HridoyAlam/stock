2ed1f13c4d76e736a8550b62ebc4de6e0c27fb96
HridoyAlam <optimus5289@gmail.com>
2022-04-30 11:33:58 +0600

    add new page

--- stocks/app/urls.py
+++ stocks/app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.home, name= "home"),
     path('about.html', views.about, name="about"),
     path('test.html', views.test, name='test'),
+    path('d_page1.html', views.page1, name='page1'),
+    path('d_page2.html', views.page2, name='page2'),
+    path('name.html', views.get_name, name='name'),
+    path('profile.html', views.profile, name='profile'),
+    
 ]
--- stocks/app/views.py
+++ stocks/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+import requests
+from .forms import NameForm
 
 # Create your views here.
 def home(request):
@@ -19,6 +22,29 @@ def about(request):
 def test(request):
     return render(request, "test.html", {})
 
+def page1(request):
+    return render(request, "d_page1.html", {})
+
+def page2(request):
+    results = request.GET['model']
+    print(results)
+    return render(request, "d_page2.html", {'model': results})
+
+def get_name(request):
+    if request.method == "POST":
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect("name")
+    else:
+        form = NameForm()
+    context = {
+        'form_key': form
+    }
+    return render(request, 'name.html', context)
+
+
+def profile(request):
+    return render(request, "profile.html", {})
 
 # pk_0f23852bc2214eecb55dea7c1ffb8cba 
 # aapl
